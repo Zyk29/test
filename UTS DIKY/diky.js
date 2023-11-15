080cb7dc2928dcce265cbd18dee34d37a9ce13d4
Rafa <138652527+Zyk29@users.noreply.github.com>
2023-11-15 08:15:08 +0700

    Add files via upload

--- UTS DIKY/diky.js
+++ UTS DIKY/diky.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector('.navbar #menu');
 const nav = document.querySelector('.navbar .navbar-top');
 
-menu.addEventListener('click', function(){
+menu.addEventListener('click',function(){
     nav.classList.toggle('active');
 });
